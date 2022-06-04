feaf6a7a750e422316f93ad341db0c08280fb3f4
bartosz.kosmala <bartosz.kosmala@olx.pl>
2022-06-04 17:13:55 +0200

    init initializr

---
+++ src/main/java/com/quizy/QuizyApplication.java
@@ -0,0 +1,13 @@
+package com.quizy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizyApplication.class, args);
+    }
+
+}
